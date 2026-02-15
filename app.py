1801ade98d58f3cbac3bda97bd17d80f8da2d47d
neerajnirmala64-eng <neerajnirmala64@gmail.com>
2026-02-15 14:54:59 +0530

    Update and rename APP.py to app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+
+model = joblib.load("salary_prediction_model.pkl")
+encoder = joblib.load("label_encoder.pkl")
+
+
+st.title("Salary prediction app")
+
+
+age = st.number_input("Age", 18,60)
+gender = st.selectbox("Gender",encoder["Gender"].classes_)
+education = st.selectbox("Education Level",encoder["Education Level"].classes_)
+job_title = st.selectbox("Job Title",encoder["Job Title"].classes_)
+years_of_exp = st.number_input("Years of Experience", 0,40)
+
+
+df = pd.dataFrame({
+    "Age":[age],
+    "Gender":[gender],
+    "Education Level":[education],
+    "Job Title":[job_title],
+    "Years of Experience":[years_of_exp]
+
+})
+
+
+if st.button("Predict"):
+    for col in encoder:
+        df[col] = encoder[col].transform(df[col])
+    prediction = model.predict(df)
+    st.success(f"Predicted Salary: {prediction[0]:,.2f}")
+
+
+
+
+
+
